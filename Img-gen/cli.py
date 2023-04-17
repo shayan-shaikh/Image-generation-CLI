8ffb3053d5f52eb737028a83e440c43a772ef9ef
Shayan Shaikh <Shayan.shaikh@oslash.com>
2023-04-17 15:43:41 +0530

    entry point redefinition wrt imggencli

--- Img-gen/cli.py
+++ Img-gen/cli.py
@@ -131,7 +131,7 @@ def apply_filter_choices(image, filter_name):
 
 
 @click.group(name='ImggenCli')
-@click.version_option(version="1.2.4")
+@click.version_option(version="1.3.1")
 def cli():
     """💠 Use this Open AI api to generate, as well as customize : edit & filter images from the cmd line."""
 
